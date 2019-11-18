5266db83576230ec8e863df67db1deadbcc24f25
Lakota Larson <lakota-larson@uiowa.edu>
2019-11-18 08:55:39 -0600

    Update, details and search is working

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask
-from flask import render_template, redirect, request, flash
+from flask import render_template, redirect, request, flash, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
 import pymysql
 import secrets
 
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='SuperSecretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = conn
+db = SQLAlchemy(app)
+'''
+#for when pushed to Azure
 conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='SuperSecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = conn
 db = SQLAlchemy(app)
-
+'''
 class lglarson_dogapp(db.Model):
     dogId = db.Column(db.Integer, primary_key=True)
     dogName = db.Column(db.String(255))
@@ -24,14 +34,28 @@ class lglarson_dogapp(db.Model):
         return "id: {0} | dog name: {1} | age: {2} | breed: {3}".format(self.id, self.dogName, self.age, self.breed)
 
 class DogForm(FlaskForm):
+    dogId = IntegerField('Dog ID: ')
     dogName = StringField('Dog Name:', validators=[DataRequired()])
-    age = StringField('Age:', validators=[DataRequired()])
+    age = IntegerField('Age:', validators=[DataRequired()])
     breed= StringField('Breed: ', validators=[DataRequired()])
 
 @app.route('/')
 def index():
     all_dogs = lglarson_dogapp.query.all()
     return render_template('index.html', dogs=all_dogs, pageTitle='Lakota\'s Dogs')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method =='POST':
+        form = request.form
+        search_value = form['search_string']
+        search = "%{0}%".format(search_value)
+        results = lglarson_dogapp.query.filter( or_(lglarson_dogapp.dogName.like(search), lglarson_dogapp.breed.like(search))).all()
+        return render_template('index.html', dogs=results, pageTitle='Lakota\'s Dogs', legend='Search Results')
+    else:
+        return redirect('/')
+
+
 
 @app.route('/add_dog', methods=['GET', 'POST'])
 def add_dog():
@@ -57,6 +81,29 @@ def delete_dog(dogId):
     else: #if it's a GET request, send them to the home page
         return redirect("/")
 
+@app.route('/dog/<int:dogId>', methods=['GET', 'POST'])
+def dog(dogId):
+    dog = lglarson_dogapp.query.get_or_404(dogId)
+    return render_template('dog.html', form=dog, pageTitle='Dog Details', legend="Dog Details")
+
+@app.route('/dog/<int:dogId>/update', methods=['GET','POST'])
+def update_dog(dogId):
+    dog = lglarson_dogapp.query.get_or_404(dogId)
+    form = DogForm()
+
+    if form.validate_on_submit():
+        dog.dogId = form.dogId.data
+        dog.dogName = form.dogName.data
+        dog.age = form.age.data
+        dog.breed = form.breed.data
+        db.session.commit()
+        return redirect('/')
+
+    form.dogId.data = dog.dogId
+    form.dogName.data = dog.dogName
+    form.age.data = dog.age
+    form.breed.data = dog.breed
+    return render_template('update_dog.html', form=form, pageTitle='Update Post',legend="Update A Dog")
 
 if __name__ == '__main__':
     app.run(debug=True)
